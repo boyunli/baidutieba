0692582907e9c1f52e58c8c705881fac5c07ae46
liling <liling@meifang.com>
2017-06-01 14:36:07 +0800

    use theano+keras

--- src/ocr/cnn.py
+++ src/ocr/cnn.py
@@ -1,74 +1,64 @@
 # coding:utf-8
+
+from __future__ import absolute_import
+from __future__ import print_function
 import numpy as np
+np.random.seed(1337)  # for reproducibility
 
-def tanh(x):  
-    return np.tanh(x)
+from keras.callbacks import ModelCheckpoint 
+from data import load_data,load_validation
+from keras.models import Sequential
+from keras.layers.core import Dense, Dropout, Activation, Flatten
+from keras.layers.convolutional import Convolution2D, MaxPooling2D
+from keras.utils import np_utils
 
-def tanh_deriv(x):  
-    return 1.0 - np.tanh(x)*np.tanh(x)
+batch_size = 128
+nb_classes = 32
+nb_epoch = 12
 
-def logistic(x):  
-    return 1/(1 + np.exp(-x))
+# input image dimensions
+img_rows, img_cols = 28, 28
+# number of convolutional filters to use
+nb_filters = 32
+# size of pooling area for max pooling
+nb_pool = 2
+# convolution kernel size
+nb_conv = 3
+import pdb
+pdb.set_trace()
+# the data, shuffled and split between tran and test sets
+data,label = load_data()
+label= np_utils.to_categorical(label, nb_classes)
 
-def logistic_derivative(x):  
-    return logistic(x)*(1-logistic(x))
+v_data,v_label = load_validation()
+v_label= np_utils.to_categorical(v_label, nb_classes)
 
 
+model = Sequential()
 
-class NeuralNetwork:   
-    def __init__(self, layers, activation='tanh'):  
-        """  
-        :param layers: A list containing the number of units in each layer.
-        Should be at least two values  
-        :param activation: The activation function to be used. Can be
-        "logistic" or "tanh"  
-        """  
-        if activation == 'logistic':  
-            self.activation = logistic  
-            self.activation_deriv = logistic_derivative  
-        elif activation == 'tanh':  
-            self.activation = tanh  
-            self.activation_deriv = tanh_deriv
-    
-        self.weights = []  
-        for i in range(1, len(layers) - 1):  
-            self.weights.append((2*np.random.random((layers[i - 1] + 1, layers[i] + 1))-1)*0.25)  
-            self.weights.append((2*np.random.random((layers[i] + 1, layers[i + 1]))-1)*0.25)
-            
-            
-    def fit(self, X, y, learning_rate=0.2, epochs=10000):         
-        X = np.atleast_2d(X)         
-        temp = np.ones([X.shape[0], X.shape[1]+1])         
-        temp[:, 0:-1] = X  # adding the bias unit to the input layer         
-        X = temp         
-        y = np.array(y)
-    
-        for k in range(epochs):  
-            i = np.random.randint(X.shape[0])  
-            a = [X[i]]
-    
-            for l in range(len(self.weights)):  #going forward network, for each layer
-                a.append(self.activation(np.dot(a[l], self.weights[l])))  #Computer the node value for each layer (O_i) using activation function
-            error = y[i] - a[-1]  #Computer the error at the top layer
-            deltas = [error * self.activation_deriv(a[-1])] #For output layer, Err calculation (delta is updated error)
-            
-            #Staring backprobagation
-            for l in range(len(a) - 2, 0, -1): # we need to begin at the second to last layer 
-                #Compute the updated error (i,e, deltas) for each node going from top layer to input layer 
-                deltas.append(deltas[-1].dot(self.weights[l].T)*self.activation_deriv(a[l]))  
-            deltas.reverse()  
-            for i in range(len(self.weights)):  
-                layer = np.atleast_2d(a[i])  
-                delta = np.atleast_2d(deltas[i])  
-                self.weights[i] += learning_rate * layer.T.dot(delta)
-                
-                
-    def predict(self, x):         
-        x = np.array(x)         
-        temp = np.ones(x.shape[0]+1)         
-        temp[0:-1] = x         
-        a = temp         
-        for l in range(0, len(self.weights)):             
-            a = self.activation(np.dot(a, self.weights[l]))         
-        return a
+model.add(Convolution2D(32, nb_conv, nb_conv,
+                        border_mode='full',
+                        input_shape=(1, img_rows, img_cols)))
+model.add(Activation('relu'))#relu
+model.add(Convolution2D(32, nb_conv, nb_conv))
+model.add(Activation('relu'))#relu
+model.add(MaxPooling2D(pool_size=(nb_pool, nb_pool)))
+model.add(Dropout(0.25))
 
+model.add(Convolution2D(64, nb_conv, nb_conv))
+model.add(Activation('relu'))
+model.add(Convolution2D(128, nb_conv, nb_conv))#64
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(nb_pool, nb_pool)))
+model.add(Dropout(0.25))
+
+
+model.add(Flatten())
+model.add(Dense(512))
+model.add(Activation('tanh'))
+model.add(Dropout(0.5))
+model.add(Dense(nb_classes))
+model.add(Activation('softmax'))
+model.compile(loss='categorical_crossentropy', optimizer='adadelta')
+checkpointer = ModelCheckpoint(filepath="./weight/cnn_tanh.h5", verbose=1, save_best_only=True)
+model.fit(data, label, batch_size=batch_size, nb_epoch=20,shuffle=True,verbose=1,show_accuracy=True,validation_data=(v_data,v_label),callbacks=[checkpointer])
